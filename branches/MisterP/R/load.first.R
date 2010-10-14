7a1310b0580ce56249e094e9de67994a6e11601c
malecki (Dr ) <malecki@gmail.com>
2010-10-14 19:58:25 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mrp@104 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/MisterP/R/load.first.R
+++
@@ -1,6 +0,0 @@
-.onAttach <- function(...) {
-  mylib <- dirname(system.file(package = "MisterP"))
-  ver <- packageDescription("MisterP", lib = mylib)$Version
-  builddate <- packageDescription("MisterP", lib = mylib)$Date
-  cat(paste("\nMisterP (Version ", ver, ", built: ", builddate, ")\n", sep = ""))
-}
