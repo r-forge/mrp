4409b0f4316e6cf2ab6ac5ec2d69465f97a74b60
malecki (Dr ) <malecki@gmail.com>
2011-12-24 14:24:53 +0000

    lowercase oops
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mrp@204 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/mrp/R/load.first.R
+++ pkg/mrp/R/load.first.R
@@ -1,6 +1,6 @@
 .onAttach <- function(...) {
-  mylib <- dirname(system.file(package = "MRP"))
-  ver <- packageDescription("MRP", lib = mylib)$Version
-  builddate <- packageDescription("MRP", lib = mylib)$Date
-  packageStartupMessage(paste("\nMRP (Version ", ver, ", built: ", builddate, ")\n", sep = ""))
+  mylib <- dirname(system.file(package = "mrp"))
+  ver <- packageDescription("mrp", lib = mylib)$Version
+  builddate <- packageDescription("mrp", lib = mylib)$Date
+  packageStartupMessage(paste("\nmrp (Version ", ver, ", built: ", builddate, ")\n", sep = ""))
 }
