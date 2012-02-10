0a385654b5530cfa233392ec22490f6374b7911e
guojq (Jiqiang Guo) <guojq28@gmail.com>
2012-02-10 19:06:57 +0000

    only mrp-vignette.pdf for pkg/mrp/inst/doc and some revisions to satisfy cran's check
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mrp@211 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/mrp/R/load.first.R
+++ pkg/mrp/R/load.first.R
@@ -1,6 +1,5 @@
 .onAttach <- function(...) {
   mylib <- dirname(system.file(package = "mrp"))
-  ver <- packageDescription("mrp", lib = mylib)$Version
-  builddate <- packageDescription("mrp", lib = mylib)$Date
-  packageStartupMessage(paste("\nmrp (Version ", ver, ", built: ", builddate, ")\n", sep = ""))
+  pkgdesc <- packageDescription("mrp") 
+  packageStartupMessage(paste("\nmrp (Version ", pkgdesc$Version, ", built: ", pkgdesc$Date, ")\n", sep = ""))
 }
