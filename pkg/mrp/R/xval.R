30044bd120d6f5ad85dfc70b8713442cbb0daeab
weiwang (Wei Wang) <thomas.wei.wang@gmail.com>
2012-03-02 02:08:27 +0000

    add sketch of xval method
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mrp@213 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/mrp/R/xval.R
@@ -0,0 +1,11 @@
+setGeneric("xval", function(object, formula=NULL, folds=4, loss.type=, sample.variables) {StandardGeneric("xval")})
+
+setMethod(f="xval",
+          signature=signature(object="xval"),
+          definition=function(object, formula=NULL, blabla){
+            ## create a list of length folds that holds different partitions
+
+            ## fit the model and calculate the 
+          }
+          
+          )
